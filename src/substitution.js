cd46ba226059f8f178d05e3a925b8d7f0be10752
Ryan Mahoney <rsmahoney211@gmail.com>
2023-01-23 16:22:44 -0600

    completed substitution function for ./src/substitution.js

--- src/substitution.js
+++ src/substitution.js
@@ -5,12 +5,43 @@
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
+  const normalAlphabet = "abcdefghijklmnopqrstuvwxyz"
 
-  function substitution(input, alphabet, encode = true) {
+  function substitution(input = "", alphabet = "", encode = true) {
     // your solution code here
-    if(alphabet.length !== 26 || )
+    if (alphabet.length !== 26 || checkForUniqueCharacters(alphabet) !== 26) return false;
+    let output = [];
+    let indexOfInputCharacters = []
+    let inputArr = input.toLowerCase().split('');
+
+    if(encode) {
+      inputArr.forEach(character => {
+        normalAlphabet.indexOf(character) > -1 ? indexOfInputCharacters.push(normalAlphabet.indexOf(character)) : indexOfInputCharacters.push(character);
+      })
+      indexOfInputCharacters.forEach(element => {
+        typeof (element) === "number" ? output.push(alphabet[element]) : output.push(element)
+      })
+      return output.join('');
+    } else {
+      inputArr.forEach(character => {
+        alphabet.indexOf(character) > -1 ? indexOfInputCharacters.push(alphabet.indexOf(character)) : indexOfInputCharacters.push(character);
+      })
+      indexOfInputCharacters.forEach(element => {
+        typeof (element) === "number" ? output.push(normalAlphabet[element]) : output.push(element);
+      })
+      return output.join('')
+    }
   }
 
+  function checkForUniqueCharacters(string) {
+    let array = [];
+    for (let i = 0; i < string.length; i++) {
+      if (!array.includes(string[i])) {
+        array.push(string[i]);
+      }
+    }
+    return array.length;
+  }
   return {
     substitution,
   };
