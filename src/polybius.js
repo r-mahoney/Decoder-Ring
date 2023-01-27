fc888780c8f38613750d16bcc20a75c016d0929b
Ryan Mahoney <rsmahoney211@gmail.com>
2023-01-27 10:56:11 -0600

    started work on polybius square

--- src/polybius.js
+++ src/polybius.js
@@ -8,6 +8,49 @@ const polybiusModule = (function () {
 
   function polybius(input, encode = true) {
     // your solution code here
+    let output = [];
+    
+  }
+
+  function encode(char, arrToPushTo) {
+    switch (char) {
+      case "A":
+        arrToPushTo.push("11");
+        break;
+      case "B":
+        arrToPushTo.push("12");
+        break;
+      case "C":
+        arrToPushTo.push("13");
+        break;
+      case "D":
+        arrToPushTo.push("14");
+        break;
+      case "E":
+        arrToPushTo.push("15");
+        break;
+      case "F":
+        arrToPushTo.push("21");
+        break;
+      case "G":
+        arrToPushTo.push("22");
+        break;
+      case "H":
+        arrToPushTo.push("23");
+        break;
+      case "I": case "J":
+        arrToPushTo.push("24");
+        break;
+      case "K":
+        arrToPushTo.push("25");
+        break;
+      case "L":
+        arrToPushTo.push("31");
+        break;
+      case "M":
+        arrToPushTo.push("32");
+        break;
+    }
   }
 
   return {
