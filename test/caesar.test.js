1797eee5fb71914194e0a8214e309c4bc972431b
Ryan Mahoney <rsmahoney211@gmail.com>
2023-01-27 22:16:38 -0600

    added 'encode' and 'decode' describes to caeser.test.js

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -3,7 +3,7 @@ const { caesar, positiveShift, negativeShift } = require("../src/caesar");
 const { expect } = require("chai");
 
 describe("**************Caesar tests written by Me**************", () => {
-  describe("caeser()", () => {
+  describe("Encoding", () => {
     it("should return false if shift is not given or is equal to 0", () => {
       const actual = caesar("", 0, true);
       expect(actual).to.be.false;
@@ -26,25 +26,27 @@ describe("**************Caesar tests written by Me**************", () => {
       const actual = "iaoowca";
       expect(actual).to.equal(expected);
     });
-    it("should decode a message by the given negative shift ", () => {
-      const expected = caesar("iaoowca", -4, false);
-      const actual = "message";
-      expect(actual).to.equal(expected);
-    });
-    it("should decode a message by the given negative shift ", () => {
-      const expected = caesar("qiwweki", 4, false);
-      const actual = "message";
-      expect(actual).to.equal(expected);
-    });
-    it("should account for spaces and special characters", () => {
-      const expected = caesar(
-        "HeLLLo mY babY, Hello my DARling, Hello MY RagTIMe GALL!!!",
-        4,
-        true
-      );
-      const actual =
-        "lippps qc fefc, lipps qc hevpmrk, lipps qc vekxmqi kepp!!!";
-      expect(actual).to.equal(expected);
+    describe("Decoding", () => {
+      it("should decode a message by the given negative shift ", () => {
+        const expected = caesar("iaoowca", -4, false);
+        const actual = "message";
+        expect(actual).to.equal(expected);
+      });
+      it("should decode a message by the given negative shift ", () => {
+        const expected = caesar("qiwweki", 4, false);
+        const actual = "message";
+        expect(actual).to.equal(expected);
+      });
+      it("should account for spaces and special characters", () => {
+        const expected = caesar(
+          "HeLLLo mY babY, Hello my DARling, Hello MY RagTIMe GALL!!!",
+          4,
+          true
+        );
+        const actual =
+          "lippps qc fefc, lipps qc hevpmrk, lipps qc vekxmqi kepp!!!";
+        expect(actual).to.equal(expected);
+      });
     });
   });
 });
